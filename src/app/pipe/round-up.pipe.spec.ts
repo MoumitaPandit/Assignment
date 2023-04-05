155441286ff660cf57076a2382aece84540e885f
Moumita <kmoumit@apac.corpdir.net>
2023-04-05 11:41:53 +0530

    Auto-generated commit [ci skip]

--- src/app/pipe/round-up.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RoundUpPipe } from './round-up.pipe';
-
-describe('RoundUpPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RoundUpPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
